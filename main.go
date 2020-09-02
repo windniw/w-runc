ad96a4f696425b7628fcedaaf6b82ed6bb273520
windniw <luxuan4@huawei.com>
2020-09-02 14:07:08 +0800

    feat: run process with namespace isolation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 // docker          run image <cmd> <params>
@@ -13,13 +14,37 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("not support command")
 	}
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v as %d \n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	cmd.Run()
+
+}
+
+func child() {
+	fmt.Printf("Running %v as %d \n", os.Args[2:], os.Getpid())
+
+	must(syscall.Sethostname([]byte("container")))
+	must(syscall.Chroot("rootfs"))
+	must(syscall.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -27,4 +52,12 @@ func run() {
 	cmd.Stderr = os.Stderr
 
 	cmd.Run()
+
+	must(syscall.Unmount("/proc", 0))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
